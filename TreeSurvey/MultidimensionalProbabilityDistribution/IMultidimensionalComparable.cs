2d19b602b42a6fa13f713fc9005fa2503c88602f
Xia Zhang <zhangxia.finance@live.cn>
2015-07-21 02:24:09 +0800

    add SampleSet

--- TreeSurvey/MultidimensionalProbabilityDistribution/IMultidimensionalComparable.cs
+++ TreeSurvey/MultidimensionalProbabilityDistribution/IMultidimensionalComparable.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace MultidimensionalProbabilityDistribution
@@ -15,6 +16,10 @@ namespace MultidimensionalProbabilityDistribution
 		/// result[i] gt 0 means this[i] gt other[i].
 		/// </summary>
 		int[] MultidimensionalCompareTo(T other);
+		/// <summary>
+		/// The number of dimensions for comparing.
+		/// </summary>
+		int ComparableDimension();
 	}
 	/// <summary>
 	/// The inferface for any comparer that compares two objects in seperate dimensions.
@@ -28,16 +33,28 @@ namespace MultidimensionalProbabilityDistribution
 		/// result[i] gt 0 means x[i] gt y[i].
 		/// </summary>
 		int[] MultidimensionalCompare(T x,T y);
+		/// <summary>
+		/// The number of dimensions for comparing.
+		/// </summary>
+		int ComparableDimension();
 	}
 	public static class MultidimensionalComparable
 	{
 		public static int[] MultidimensionalCompareTo<T>(this IComparable<T> me,T other)
-		{
-			return new int[] { me.CompareTo(other) };
-		}
+		{ return new int[] { me.CompareTo(other) }; }
+		public static int ComparableDimension<T>(this IComparable<T> me)
+		{ return 1; }
+		public static int[] MultidimensionalCompareTo(this IComparable me,object other)
+		{ return new int[] { me.CompareTo(other) }; }
+		public static int ComparableDimension(this IComparable me)
+		{ return 1; }
 		public static int[] MultidimensionalCompare<T>(this IComparer<T> me,T x,T y)
-		{
-			return new int[] { me.Compare(x,y) };
-		}
+		{ return new int[] { me.Compare(x,y) }; }
+		public static int ComparableDimension<T>(this IComparer<T> me)
+		{ return 1; }
+		public static int[] MultidimensionalCompare(this IComparer me,object x,object y)
+		{ return new int[] { me.Compare(x,y) }; }
+		public static int ComparableDimension(this IComparer me)
+		{ return 1; }
 	}
 }
---
+++ TreeSurvey/MultidimensionalProbabilityDistribution/SampleSet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultidimensionalProbabilityDistribution
+{
+	public abstract class SubSampleSet<T> where T : IMultidimensionalComparable<T>
+	{
+		public const int IndexTrigger = 1024;
+		public const int IndexSegment = 16;
+		public const int IndexLevel = 8;
+		public enum Type
+		{
+			Index, Bucket
+		}
+		public abstract Type SubSampleSetType();
+	}
+	public class IndexSubSampleSet<T>:SubSampleSet<T> where T : IMultidimensionalComparable<T>
+	{
+		public IndexSubSampleSet(BucketSubSampleSet<T> bucketSubSampleSet)
+		{
+			throw new NotImplementedException();
+		}
+		public override Type SubSampleSetType()
+		{ return SubSampleSet<T>.Type.Index; }
+	}
+	public class BucketSubSampleSet<T>:SubSampleSet<T> where T : IMultidimensionalComparable<T>
+	{
+		public override Type SubSampleSetType()
+		{ return SubSampleSet<T>.Type.Bucket; }
+		Dictionary<T,double> bucket = new Dictionary<T,double>(IndexTrigger);
+		SubSampleSet<T> Add(T item,int num)
+		{
+			if (bucket.ContainsKey(item))
+			{
+				double temp = bucket[item] + num;
+				bucket[item] = temp < 0 ? 0 : temp;
+				return this;
+			}
+			if (bucket.Count == IndexTrigger)
+				return new IndexSubSampleSet<T>(this);
+			bucket.Add(item,num > 0 ? num : 1);
+			return this;
+		}
+	}
+	public class SampleSet<T> where T : IMultidimensionalComparable<T>
+	{
+		SubSampleSet<T> storage;
+	}
+}
